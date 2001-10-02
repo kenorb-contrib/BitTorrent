3b843d4579dca855b790e310c991db6d301e688f
bram_cohen <bram_cohen>
2001-10-02 01:20:41 +0000

    fixed bug and changed name of bittorrent download library - python packages can't have hyphens in them (oops)

--- btfetch.py
+++ btfetch.py
@@ -2,4 +2,4 @@ from BitTorrent.download import downloadurl
 from threading import Event
 
 def download(url, filename):
-    return downloadurl(url, lambda x, f = filename: f, lambda p: None, Event(), {})
+    return downloadurl(url, lambda x, f = filename: f, lambda p, q: None, Event(), {})
