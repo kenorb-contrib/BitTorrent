fa56c7d773fbc08a11c80fe3fd4c3b13a3c790b8
bram_cohen <bram_cohen>
2001-10-04 07:49:31 +0000

    made test of PublisherThrottler (it passed right off the bad)

--- BitTorrent/PublisherThrottler.py
+++ BitTorrent/PublisherThrottler.py
@@ -1,6 +1,9 @@
 # written by Bram Cohen
 # this file is public domain
 
+true = 1
+false = 0
+
 class Throttler:
     def __init__(self, max_uploads):
         self.max_uploads = max_uploads
@@ -34,3 +37,82 @@ class Throttler:
 
     def data_sent_out(self, up, amount):
         pass
+
+class DummyConnection:
+    def __init__(self, id, t):
+        self.throttled = false
+        self.uploading = false
+        self.id = id
+        self.t = t
+
+    def start_uploading(self):
+        self.uploading = true
+        self.t.upload_started(self)
+
+    def stop_uploading(self):
+        self.uploading = false
+        self.t.upload_stopped(self)
+        
+    def is_uploading(self):
+        return self.uploading
+
+    def get_id(self):
+        return self.id
+
+    def is_throttled(self):
+        return self.throttled
+
+    def throttle(self):
+        self.throttled = true
+        
+    def unthrottle(self):
+        self.throttled = false
+
+def test():
+    t = Throttler(2)
+    da = DummyConnection('a', t)
+    db = DummyConnection('b', t)
+    dc = DummyConnection('c', t)
+    dd = DummyConnection('d', t)
+    t.upload_connected(da)
+    assert not da.throttled
+    
+    t.upload_connected(db)
+    assert not da.throttled
+    assert not db.throttled
+    
+    t.upload_connected(dc)
+    assert not da.throttled
+    assert not db.throttled
+    assert not dc.throttled
+
+    da.start_uploading()
+    assert not da.throttled
+    assert not db.throttled
+    assert not dc.throttled
+    
+    db.start_uploading()
+    assert not da.throttled
+    assert not db.throttled
+    assert dc.throttled
+    
+    db.stop_uploading()
+    assert not da.throttled
+    assert not db.throttled
+    assert not dc.throttled
+    
+    dc.start_uploading()
+    assert not da.throttled
+    assert db.throttled
+    assert not dc.throttled
+    
+    t.upload_connected(dd)
+    assert not da.throttled
+    assert db.throttled
+    assert not dc.throttled
+    assert dd.throttled
+    
+    t.upload_disconnected(dc)
+    assert not da.throttled
+    assert not db.throttled
+    assert not dd.throttled
