6e0888401d0e99da925fd6e432e3bb99710660cd
bram_cohen <bram_cohen>
2001-11-22 01:43:10 +0000

    added tests for Uploader
    unit testing is now complete, no I just gotta put rate-measuring functionality
    back in and I'll finally be able to do a new release

--- BitTorrent/Uploader.py
+++ BitTorrent/Uploader.py
@@ -16,7 +16,7 @@ class Upload:
 
     def send_intro(self):
         self.connection.send_message({'type': 'I have', 
-            'blobs': blobs.get_list_of_blobs_I_have()})
+            'blobs': self.blobs.get_list_of_blobs_I_have()})
 
     def got_done(self):
         if self.interested:
@@ -66,7 +66,7 @@ class Upload:
         
     def unchoke(self):
         self.choked = false
-        self.fix_choke
+        self.fix_choke()
         
     def is_choked(self):
         return self.choked
@@ -83,22 +83,196 @@ class Upload:
         del self.choker
         del self.blobs
 
-"""
+class DummyConnection:
+    def __init__(self, events):
+        self.events = events
+        self.flushed = true
+
+    def send_message(self, message):
+        self.events.append(message)
+    
+    def is_flushed(self):
+        return self.flushed
+
+class DummyChoker:
+    def __init__(self, events):
+        self.events = events
+
+    def interested(self, connection):
+        self.events.append('interested')
+    
+    def not_interested(self, connection):
+        self.events.append('not interested')
+
+class DummyBlobs:
+    def __init__(self, blobs):
+        self.blobs = blobs
+
+    def get_list_of_blobs_I_have(self):
+        return self.blobs.keys()
+    
+    def get_slice(self, blob, begin, length):
+        if not self.blobs.has_key(blob):
+            return None
+        return self.blobs[blob][begin : begin + length]
 
 def test_skip_over_choke():
-    queue requests, choke and unchoke, flush
-    assert requests still sent
-    send another, asserts gets through immediately
+    events = []
+    connection = DummyConnection(events)
+    choker = DummyChoker(events)
+    blobs = DummyBlobs({'a' * 20: 'abcd'})
+    upload = Upload(connection, choker, blobs)
+    assert not upload.is_choked()
+    assert not upload.is_interested()
+    
+    upload.send_intro()
+    assert events == [{'type': 'I have', 'blobs': ['a' * 20]}]
+    del events[:]
 
-def test_transitions_clockwise():
-    make thing
-    transition to interested, choked, uninterested, unchoked
+    connection.flushed = false
+    upload.got_send({'type': 'send', 'blob': 'a' * 20, 'begin': 1, 'length': 2})
+    assert events == ['interested']
+    del events[:]
+    assert not upload.is_choked()
+    assert upload.is_interested()
 
-def test_transitions_counterclockwise():
-    make thing
-    transition to choked, interested, unchoked, uninterested
+    upload.choke()
+    assert events == []
+    assert upload.is_choked()
+    assert upload.is_interested()
+    
+    upload.unchoke()
+    assert events == []
+    assert not upload.is_choked()
+    assert upload.is_interested()
+
+    connection.flushed = true
+    upload.flushed()
+    assert events == [{'type': 'slice', 'blob': 'a' * 20, 'slice': 'bc', 'begin': 1}]
+    del events[:]
+    assert not upload.is_choked()
+    assert upload.is_interested()
+
+    upload.got_send({'type': 'send', 'blob': 'a' * 20, 'begin': 1, 'length': 2})
+    assert events == [{'type': 'slice', 'blob': 'a' * 20, 'slice': 'bc', 'begin': 1}]
+    del events[:]
+    assert not upload.is_choked()
+    assert upload.is_interested()
 
 def test_received_blob():
-    call received_blob
+    events = []
+    connection = DummyConnection(events)
+    choker = DummyChoker(events)
+    blobs = DummyBlobs({'a' * 20: 'abcd'})
+    upload = Upload(connection, choker, blobs)
+    assert not upload.is_choked()
+    assert not upload.is_interested()
+    
+    upload.received_blob('a' * 20)
+    assert events == [{'type': 'I have', 'blobs': ['a' * 20]}]
 
-"""
+def test_get_bad_slice():
+    events = []
+    connection = DummyConnection(events)
+    choker = DummyChoker(events)
+    blobs = DummyBlobs({})
+    upload = Upload(connection, choker, blobs)
+    assert not upload.is_choked()
+    assert not upload.is_interested()
+
+    upload.got_send({'type': 'send', 'blob': 'a' * 20, 'begin': 0, 'length': 2})
+    assert events == ['interested']
+    assert not upload.is_choked()
+    assert upload.is_interested()
+
+def test_transitions_clockwise():
+    events = []
+    connection = DummyConnection(events)
+    choker = DummyChoker(events)
+    blobs = DummyBlobs({'a' * 20: 'abcd'})
+    upload = Upload(connection, choker, blobs)
+    assert not upload.is_choked()
+    assert not upload.is_interested()
+
+    upload.got_interested()
+    assert events == ['interested']
+    del events[:]
+    assert not upload.is_choked()
+    assert upload.is_interested()
+
+    upload.choke()
+    assert events == [{'type': 'choke'}]
+    del events[:]
+    assert upload.is_choked()
+    assert upload.is_interested()
+
+    upload.got_send({'type': 'send', 'blob': 'a' * 20, 'begin': 1, 'length': 2})
+    assert events == []
+    assert upload.is_choked()
+    assert upload.is_interested()
+
+    upload.got_done()
+    assert events == ['not interested']
+    del events[:]
+    assert upload.is_choked()
+    assert not upload.is_interested()
+
+    upload.unchoke()
+    assert events == [{'type': 'unchoke'}]
+    del events[:]
+    assert not upload.is_choked()
+    assert not upload.is_interested()
+
+    upload.got_send({'type': 'send', 'blob': 'a' * 20, 'begin': 1, 'length': 2})
+    assert events == [{'type': 'slice', 'blob': 'a' * 20, 'slice': 'bc', 'begin': 1}, 'interested']
+    del events[:]
+    assert not upload.is_choked()
+    assert upload.is_interested()
+
+def test_transitions_counterclockwise():
+    events = []
+    connection = DummyConnection(events)
+    choker = DummyChoker(events)
+    blobs = DummyBlobs({'a' * 20: 'abcd'})
+    upload = Upload(connection, choker, blobs)
+    assert not upload.is_choked()
+    assert not upload.is_interested()
+
+    upload.choke()
+    assert events == [{'type': 'choke'}]
+    del events[:]
+    assert upload.is_choked()
+    assert not upload.is_interested()
+
+    upload.got_send({'type': 'send', 'blob': 'a' * 20, 'begin': 1, 'length': 2})
+    assert events == ['interested']
+    del events[:]
+    assert upload.is_choked()
+    assert upload.is_interested()
+
+    upload.got_interested()
+    assert events == []
+    assert upload.is_choked()
+    assert upload.is_interested()
+
+    upload.unchoke()
+    assert events == [{'type': 'unchoke'}]
+    del events[:]
+    assert not upload.is_choked()
+    assert upload.is_interested()
+
+    upload.got_done()
+    assert events == ['not interested']
+    del events[:]
+    assert not upload.is_choked()
+    assert not upload.is_interested()
+
+    upload.got_done()
+    assert events == []
+    assert not upload.is_choked()
+    assert not upload.is_interested()
+
+    upload.got_interested()
+    assert events == ['interested']
+    assert not upload.is_choked()
+    assert upload.is_interested()
