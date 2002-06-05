6067c40ae76f13987209e1e9327d5358c7360d7d
bram_cohen <bram_cohen>
2002-06-05 17:07:22 +0000

    changed version numbers

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup, Extension
 
 setup(
     name = "BitTorrent",
-    version = "2.8.0",
+    version = "2.8.1",
     author = "Bram Cohen",
     author_email = "<bram@bitconjurer.org>",
     url = "http://www.bitconjurer.org/BitTorrent/",
