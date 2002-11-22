b5718d80c1216e765c74a8377422effd9b0dc2c7
bram_cohen <bram_cohen>
2002-11-22 04:04:16 +0000

    made everything explicitly use python2, since that seems to be reasonably cross platform (works in both redhat and debian)

--- btdownloadgui.py
+++ btdownloadgui.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 # Written by Bram Cohen and Myers Carpenter
 # see LICENSE.txt for license information
--- btshowmetainfo.py
+++ btshowmetainfo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 # Written by Henry 'Pi' James
 # see LICENSE.txt for license information
