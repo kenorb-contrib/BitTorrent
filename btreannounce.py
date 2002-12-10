5b6dac152a3fd844bfeceb5f530fffd1a35eecfe
bram_cohen <bram_cohen>
2002-12-10 06:52:52 +0000

    made reannounce be nicer as a command line tool

--- btreannounce.py
+++ btreannounce.py
@@ -1,33 +1,22 @@
 #!/usr/bin/env python2
 
-# Written by Henry 'Pi' James
+# Written by Henry 'Pi' James and Bram Cohen
 # see LICENSE.txt for license information
 
-from sys import *
-from os.path import *
-from sha import *
-from BitTorrent.bencode import *
+from sys import argv
+from BitTorrent.bencode import bencode, bdecode
 
-NAME, EXT = splitext(basename(argv[0]))
-VERSION = '20021119'
+if len(argv) < 3:
+    print '%s http://new.uri:port/announce file1.torrent file2.torrent' % argv[0]
+    print
+    exit(2) # common exit code for syntax error
 
-print '%s %s - change the annoucement URI in a .torrent file' % (NAME, VERSION)
-print
-
-if len(argv) != 3:
-  print '%s file.torrent http://new.uri:port/announce' % argv[0]
-  print
-  exit(2) # common exit code for syntax error
-
-metainfo_file = open(argv[1], 'rb')
-metainfo = bdecode(metainfo_file.read())
-metainfo_file.close()
-print 'old announce: %s' % metainfo['announce']
-metainfo['announce'] = argv[2]
-print 'new announce: %s' % metainfo['announce']
-metainfo_file = open(argv[1], 'wb')
-metainfo_file.write(bencode(metainfo))
-metainfo_file.close
-print
-print 'done.'
-print
+for f in argv[2:]:
+    h = open(f, 'rb')
+    metainfo = bdecode(metainfo_file.read())
+    h.close()
+    print 'old announce for %s: %s' % (f, metainfo['announce'])
+    metainfo['announce'] = argv[1]
+    h = open(argv[1], 'wb')
+    h.write(bencode(metainfo))
+    h.close()
