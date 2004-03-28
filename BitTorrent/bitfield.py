200cbfc92d28b76ef7ff854b19511e88a3b64b31
bram_cohen <bram_cohen>
2004-03-28 15:30:49 +0000

    sped up bitfield

--- BitTorrent/bitfield.py
+++ BitTorrent/bitfield.py
@@ -1,31 +1,38 @@
-# Written by Bram Cohen
+# Written by Bram Cohen, Uoti Urpala, and John Hoffman
 # see LICENSE.txt for license information
 
-def booleans_to_bitfield(booleans):
+def _int_to_booleans(x):
     r = []
-    for i in xrange(0, len(booleans), 8):
-        v = 0
-        p = 0x80
-        for j in booleans[i:i+8]:
-            if j:
-                v |= p
-            p >>= 1
-        r.append(chr(v))
-    return ''.join(r)
+    for i in range(8):
+        if x & 0x80:
+            r.append(True)
+        else:
+            r.append(False)
+        x <<= 1
+    return tuple(r)
+
+lookup_table = [_int_to_booleans(i) for i in range(256)]
+
+reverse_lookup_table = {}
+for i in xrange(256):
+    reverse_lookup_table[lookup_table[i]] = chr(i)
+
+def booleans_to_bitfield(booleans):
+    t = reverse_lookup_table
+    s = len(booleans) % 8
+    r = ''.join([t[tuple(booleans[x:x+8])] for x in xrange(0, len(booleans) - s, 8)])
+    if s:
+        r += t[tuple(booleans[-s:] + ([0] * (8-s)))]
+    return r
 
 def bitfield_to_booleans(bitfield, l):
     extra = len(bitfield) * 8 - l
     if extra < 0 or extra >= 8:
         return None
+    t = lookup_table
     r = []
     for c in bitfield:
-        v = ord(c)
-        for i in xrange(8):
-            if v & 0x80 != 0:
-                r.append(True)
-            else:
-                r.append(False)
-            v <<= 1
+        r.extend(t[ord(c)])
     if extra > 0:
         if r[-extra:] != [0] * extra:
             return None
